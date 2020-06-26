87269365d86f0ef7b0624565b6f46c749510563e
STR1Z <weihengjiang64@hotmail.com>
2020-06-26 15:00:06 -0400

    Initial commit

---
+++ compiler/index.js
@@ -0,0 +1,189 @@
+const { parse: parseHtml, TextNode } = require("node-html-parser");
+const fs = require("fs");
+const nodepath = require("path");
+
+const jsonify = (current) => {
+  if (!current.tagName) return { TextNode: true, rawText: current.rawText };
+  return {
+    tagName: current.tagName,
+    attributes: current.attributes ? current.attributes : {},
+    childNodes: current.childNodes.map((n) => jsonify(n)),
+    rawText: current.rawText,
+    innerHTML: current.innerHTML,
+    outerHTML: current.outerHTML,
+  };
+};
+
+const copy = (object) => JSON.parse(JSON.stringify(object));
+
+const parseFunction = (trace, node, data) => [
+  node.rawText
+    .split("\n")
+    .map((v) => v.trim())
+    .join("")
+    .replace(/EXTRACT\.(\w+)\.HERE/, (_, m) => {
+      if (!data[m]) return JSON.stringify(createError(`Cannot extract inexistant component '${m}'`, trace));
+      if (data[m].type === "direct") return JSON.stringify(compile(trace, copy(data[m].node), data, compile(trace, node, data, {}, m), m));
+      else if (data[m].type === "file") {
+        return JSON.stringify(fromFile(data[m].path, compile(trace, node, data, {}, m), trace.dev_mode));
+      }
+    }),
+];
+
+const firstAttr = (node) => Object.keys(node.attributes)[0];
+
+const shiftAttr = (node) => {
+  let name = firstAttr(node);
+  delete node.attributes[name];
+  return name;
+};
+
+const compile = (trace, current, data, caller, isComponent) => {
+  let res = {
+    tag: current.tagName,
+    inner: [],
+    attr: current.attributes,
+    name: isComponent,
+    init: [],
+  };
+
+  if (!current.attributes) console.log(current);
+  if (res.attr.raw !== undefined) {
+    delete res.attr.raw;
+    res.inner.push(current.innerHTML);
+  } else
+    for (let node of current.childNodes) {
+      if (node.TextNode) {
+        let text = res.attr.notrim !== undefined ? node.rawText : node.rawText.trim();
+        if (text) res.inner.push(text);
+      } else
+        switch (node.tagName) {
+          case "use": {
+            let key = shiftAttr(node);
+            if (key == undefined) return createError("Missing reference name in object utilization or there was a component loop.", { ...trace, node });
+            if (!data[key]) return createError(`Component '${key}' does not exist.`, { ...trace, node });
+            if (data[key].type === "direct") res.inner.push(compile(trace, copy(data[key].node), data, compile(trace, node, data, caller, key), key));
+            else if (data[key].type === "file") {
+              res.inner.push(fromFile(data[key].path, compile(trace, node, data, caller, key), trace.dev_mode));
+            }
+            break;
+          }
+          case "super": {
+            res.inner.push(...caller.inner);
+            res.attr = { ...res.attr, ...caller.attr };
+            break;
+          }
+          case "script": {
+            if (isComponent && shiftAttr(node) === "init") res.init.push(parseFunction({ ...trace, node }, node, data)[0]);
+            else res.inner.push(parseFunction({ ...trace, node }, node, data));
+            break;
+          }
+          default: {
+            res.inner.push(compile(trace, node, data, caller));
+            break;
+          }
+        }
+    }
+  if (caller.init && isComponent) res.init.push(...caller.init);
+  if (!res.init[0]) delete res.init;
+  if (Object.keys(res.attr).length === 0) delete res.attr;
+  return res;
+};
+
+const createError = (message, trace) => {
+  console.log(trace);
+  console.log(`\x1b[31mInassembly Compile Error: \n${message} \nat ${nodepath.join(__dirname, trace.path)}\n\x1b[36m${trace.node.outerHTML}\x1b[0m\n`);
+  return trace.dev_mode
+    ? {
+        tag: "div",
+        attr: { style: "font-family: monospace; color: red; " },
+        inner: [
+          "Inassembly Compile Error:",
+          { tag: "br" },
+          message,
+          { tag: "br" },
+          `at ${nodepath.join(__dirname, trace.path)}`,
+          { tag: "br" },
+          { tag: "span", attr: { style: "color: dodgerblue;" }, inner: [trace.node.outerHTML] },
+        ],
+      }
+    : { tag: "span", inner: ["An error occured while creating this view. Sorry."] };
+};
+
+const fromFile = (path, caller = {}, dev_mode = false) => {
+  let content;
+  path = path.endsWith(".html") ? path : path + ".html";
+  try {
+    content = fs.readFileSync(path);
+  } catch {
+    console.log(`\x1b[31mInassembly Compile Error: \nFailed to parse content from an inexistant file\npath: ${nodepath.join(__dirname, path)}\x1b[0m`);
+  }
+  let root = parseHtml(content, { script: true, pre: true, style: true });
+  let data = {},
+    exported,
+    export_node;
+
+  for (let node of root.childNodes) {
+    if (!(node instanceof TextNode))
+      switch (node.tagName) {
+        case "import": {
+          if (firstAttr(node) == undefined) return createError("Missing reference name in component importation", { node, path, dev_mode });
+          if (!node.rawText) return createError("Missing file path in component importation", { node, path, dev_mode });
+          data[shiftAttr(node)] = { type: "file", path: node.rawText };
+          break;
+        }
+        case "export": {
+          if (firstAttr(node) == undefined) return createError("Missing reference name in component exportation", { node, path, dev_mode });
+          exported = shiftAttr(node);
+          export_node = node;
+          break;
+        }
+        default: {
+          if (firstAttr(node) == undefined) return createError("Missing reference name in component declaration", { node, path, dev_mode });
+          data[shiftAttr(node)] = { type: "direct", node: jsonify(node) };
+        }
+      }
+  }
+  if (!exported) {
+    console.log(`\x1b[33mMissing component exportation\nat ${nodepath.join(__dirname, path)}\x1b[0m\n`);
+    return {};
+  }
+
+  if (!data[exported]) return createError(`Component '${exported}' does not exist.`, { node: export_node, path, dev_mode });
+  if (data[exported].type !== "direct") return createError(`Cannot export directly imported component`, { node: export_node, path, dev_mode });
+  return compile({ path, dev_mode }, data[exported].node, data, caller, exported);
+};
+
+function byteLength(str) {
+  var s = str.length;
+  for (var i = str.length - 1; i >= 0; i--) {
+    var code = str.charCodeAt(i);
+    if (code > 0x7f && code <= 0x7ff) s++;
+    else if (code > 0x7ff && code <= 0xffff) s += 2;
+    if (code >= 0xdc00 && code <= 0xdfff) i--;
+  }
+  return s;
+}
+
+const v2Adapter = (object) => {
+  if (typeof object == "string") return object;
+  if (object instanceof Array) return object;
+  let res = [];
+  if (object.name) res.push(...(object.init ? object.init : []));
+  res.push(object.tag || "div");
+  if (object.attr) res.push(object.attr);
+  for (let node of object.inner) res.push(v2Adapter(node));
+  return res;
+};
+
+exports = {
+  devassemble: (path) => {
+    let prev = Date.now();
+    let res = v2Adapter(fromFile(path, {}, true));
+    let after = Date.now();
+    let size = byteLength(JSON.stringify(res));
+    console.log(`\x1b[32mCompiled ${path}\x1b[36m\nwith size of ${size / 1000}kB in ${after - prev}ms\x1b[0m\n`);
+    return res;
+  },
+  assemble: (path) => v2Adapter(fromFile(path)),
+};
---
+++ inassembly.js
@@ -0,0 +1,235 @@
+Inassembly = {
+  fetch: async (url, postData) => {
+    if (postData) return await fetch(url, { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.statestringify(postData) }).then((response) => response.json());
+    return await fetch(url).then((response) => response.json());
+  },
+  fetchCache: {},
+  fetchAndMount: (url, parentDom = document.body, cacheProfile = "NONE") => {
+    let profile = cacheProfile.toUpperCase();
+    if (profile !== "NONE")
+      if (profile == "PAGE" && Inassembly.fetchCache[url]) return new Inassembly.Component(Inassembly.fetchCache[url]).mountDom(parentDom);
+      else if (profile == "SESSION" && sessionStorage["fetchCache:" + url]) return new Inassembly.Component(JSON.parse(sessionStorage["fetchCache:" + url])).mountDom(parentDom);
+      else if (profile == "LOCAL" && localStorage["fetchCache:" + url]) return new Inassembly.Component(JSON.parse(localStorage["fetchCache:" + url])).mountDom(parentDom);
+    Inassembly.fetch(url).then((data) => {
+      if (profile == "NONE") {
+        if (Inassembly.fetchCache[url]) delete Inassembly.fetchCache[url];
+        else if (sessionStorage["fetchCache:" + url]) delete sessionStorage["fetchCache:" + url];
+        else if (localStorage["fetchCache:" + url]) delete localStorage["fetchCache:" + url];
+      } else if (profile == "PAGE") Inassembly.fetchCache[url] = data;
+      else if (profile == "SESSION") sessionStorage.setItem("fetchCache:" + url, JSON.stringify(data));
+      else if (profile == "LOCAL") localStorage.setItem("fetchCache:" + url, JSON.stringify(data));
+      new Inassembly.Component(data).mountDom(parentDom);
+    });
+  },
+  Component: class {
+    constructor(args, parent) {
+      this.template;
+      this.initializers = [];
+      this.autoUpdate = true;
+      this.parent = parent || null;
+      for (let i = 0; i < args.length; i++) {
+        if (/^\w+$/.test(args[i])) {
+          this.template = args.slice(i);
+          break;
+        } else this.initializers.push(args[i]);
+      }
+      this.state = {};
+      this.onMount = () => {};
+      this.onNode = () => {};
+      for (let i of this.initializers) new Function(i).bind(this)();
+      this.state = new Proxy(this.state, {
+        set: (obj, key, value) => {
+          obj[key] = value;
+          if (this.autoUpdate) this.updateDom();
+          return true;
+        },
+      });
+      this.node = new Inassembly.Node(this, this.template);
+      this.onNode(this.node);
+    }
+
+    mountDom(parentDom) {
+      parentDom.appendChild(this.node.createDom());
+    }
+
+    updateDom() {
+      let newVirtualNode = new Inassembly.Node(this, this.template);
+      this.node.updateDom(newVirtualNode);
+    }
+
+    remountDom() {
+      let old = this.node.current;
+      this.parentNode.current.replaceChild(this.node.createDom(), old);
+    }
+
+    createDom() {
+      let dom = this.node.createDom();
+      this.onMount(dom);
+      return dom;
+    }
+  },
+  Node: class {
+    constructor(component, args) {
+      this.component = component;
+      let isNotObject = (e) => ["string", "boolean", "number"].includes(typeof e);
+      if (args instanceof Array) {
+        this.type = "element";
+        this.tagName = args[0];
+        let attributes = args[1] instanceof Array || isNotObject(args[1]) ? {} : args[1];
+        this.attributes = {};
+        for (let key in attributes) this.attributes[key] = Inassembly.Node.textParser(attributes[key], this.component);
+        this.childNodes = [];
+        let children = args[1] instanceof Array || isNotObject(args[1]) ? args.slice(1) : args.slice(2);
+        for (let child of children) {
+          if (isNotObject(child)) this.appendChild(new Inassembly.Node(component, String(child)));
+          else if (child instanceof Array) {
+            if (/^\w+$/.test(child[0])) this.appendChild(new Inassembly.Node(component, child));
+            else if (child.length == 1) {
+              let returned = new Function("self", ...child).bind(this, component)();
+              if (isNotObject(returned)) this.appendChild(new Inassembly.Node(component, String(returned)));
+              else if (returned) this.appendChild(new Inassembly.Node(component, returned));
+              else this.appendChild(new Inassembly.Node(component)); // void
+            } else this.appendChild(new Inassembly.Component(child, this.component));
+          }
+        }
+      } else if (isNotObject(args)) {
+        this.type = "text";
+        this.data = Inassembly.Node.textParser(args, this.component);
+      } else this.type = "void";
+    }
+
+    appendChild() {
+      for (let node of arguments) node.parentNode = this;
+      this.childNodes.push(...arguments);
+    }
+    createDom() {
+      switch (this.type) {
+        case "element": {
+          this.current = document.createElement(this.tagName);
+          for (let key in this.attributes) this.current.setAttribute(key, this.attributes[key]);
+          for (let node of this.childNodes) this.current.appendChild(node.createDom());
+          break;
+        }
+        case "text": {
+          this.current = document.createTextNode(this.data);
+          break;
+        }
+        default: {
+          this.current = document.createTextNode("");
+          break;
+        }
+      }
+      this.current.vnode = this;
+      this.current.self = this.component;
+      return this.current;
+    }
+
+    cloneAndReplace(updatedVirtualNode) {
+      let oldDom = this.current;
+      for (let key in this) delete this[key];
+      for (let key in updatedVirtualNode) this[key] = updatedVirtualNode[key];
+      oldDom.parentNode.replaceChild(this.createDom(), oldDom);
+      console.log(oldDom, this.current);
+    }
+
+    updateDom(updatedVirtualNode) {
+      if (this.type !== updatedVirtualNode.type) this.cloneAndReplace(updatedVirtualNode);
+      if (this.type == "element") {
+        if (this.childNodes.length !== updatedVirtualNode.childNodes.length) {
+          this.cloneAndReplace(updatedVirtualNode);
+        } else {
+          for (let key in updatedVirtualNode.attributes)
+            if (this.attributes[key] !== updatedVirtualNode.attributes[key]) {
+              this.current.setAttribute(key, updatedVirtualNode.attributes[key]);
+              this.attributes[key] = updatedVirtualNode.attributes[key];
+            }
+          for (let i = 0; i < this.childNodes.length; i++) {
+            this.childNodes[i].updateDom(updatedVirtualNode.childNodes[i]);
+          }
+        }
+      } else if (this.type == "text" && this.data !== updatedVirtualNode.data) {
+        this.current.data = updatedVirtualNode.data;
+        this.data = updatedVirtualNode.data;
+      }
+    }
+
+    static textParser(text, component) {
+      return text.replace(/_(\w+)_/g, (_, m) => component.state[m]).replace(/\(\((\w+)\)\)/, (_, m) => component.methods[m]());
+    }
+  },
+
+  // used in javascript
+  createElement: (...args) => args,
+  initializers: {
+    store: (component) => {
+      if (component.node.attributes.saveAs) {
+        Inassembly.data.store[component.attr.saveAs] = component.inner[0];
+        component.nobuild = true;
+      } else if (component.attr.loadFrom) {
+        let inner = Inassembly.copy(component.inner);
+        component.inherit(Inassembly.data.store[component.attr.loadFrom]);
+        component.onbuild = () => {
+          let e = component.dom.querySelector(".super");
+          if (e) e.append(component.build({ tag: "div", inner }));
+        };
+      }
+    },
+    link: (component) => {
+      component.onNode = (node) => {
+        let attributes = node.attributes;
+        attributes.href = attributes.to;
+        attributes.onclick =
+          " if (!(location.pathname).startsWith(this.getAttribute('to'))) {history.pushState({}, null, this.attributes.to.value); document.documentElement.scrollTop = 0 }; return false";
+      };
+    },
+    route: (component) => {
+      component.onNode = (node) => {
+        if (node.attributes.exact) console.log("exact exists");
+        let childNodes = node.childNodes;
+
+        let verif = node.attributes.exactPath ? () => location.pathname == node.attributes.exactPath : () => location.pathname.startsWith(node.attributes.path);
+        if (verif()) {
+          if (!node.childNodes[0]) node.childNodes = childNodes;
+        } else if (node.childNodes[0]) node.childNodes = [];
+        window.addEventListener("locationchange", () => {
+          if (verif()) {
+            if (!node.childNodes[0]) {
+              node.childNodes = childNodes;
+              component.remountDom();
+            }
+          } else if (node.childNodes[0]) {
+            node.childNodes = [];
+            component.remountDom();
+          }
+        });
+      };
+    },
+    // route: (component) => {},
+    fetch: (component) => {
+      component.onMount = () => {
+        Inassembly.fetchAndMount(component.node.attributes.url, component.node.current, component.node.attributes.cache);
+      };
+    },
+  },
+  data: {
+    store: {},
+  },
+};
+
+history.pushState = ((f) =>
+  function pushState() {
+    var ret = f.apply(this, arguments);
+    window.dispatchEvent(new Event("locationchange"));
+    return ret;
+  })(history.pushState);
+
+history.replaceState = ((f) =>
+  function replaceState() {
+    var ret = f.apply(this, arguments);
+    window.dispatchEvent(new Event("locationchange"));
+    return ret;
+  })(history.replaceState);
+
+window.addEventListener("popstate", () => {
+  window.dispatchEvent(new Event("locationchange"));
+});
